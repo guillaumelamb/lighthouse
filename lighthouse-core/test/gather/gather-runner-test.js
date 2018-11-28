345d3e4e729e3e2330e95c5fd15f2047d1d2e45d
Shane Exterkamp <shaneexterkamp5@gmail.com>
2018-11-28 10:55:05 -0800

    Collected new strings, and fixed sec-msg test.

--- lighthouse-core/test/gather/gather-runner-test.js
+++ lighthouse-core/test/gather/gather-runner-test.js
@@ -708,7 +708,7 @@ describe('GatherRunner', function() {
       const insecureSecurityState = {
         explanations: [
           {
-            description: 'reason 1.',
+            description: 'reason 1',
             securityState: 'insecure',
           },
           {
@@ -716,7 +716,7 @@ describe('GatherRunner', function() {
             securityState: 'info',
           },
           {
-            description: 'reason 2.',
+            description: 'reason 2',
             securityState: 'insecure',
           },
         ],
@@ -729,7 +729,7 @@ describe('GatherRunner', function() {
         assert.equal(err.message, 'INSECURE_DOCUMENT_REQUEST');
         assert.equal(err.code, 'INSECURE_DOCUMENT_REQUEST');
         /* eslint-disable-next-line max-len */
-        expect(err.friendlyMessage).toBeDisplayString('The URL you have provided does not have valid security credentials. reason 1. reason 2.');
+        expect(err.friendlyMessage).toBeDisplayString('The URL you have provided does not have valid security credentials. reason 1 reason 2.');
       }
     });
   });
